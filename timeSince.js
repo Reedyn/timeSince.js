08460174e74120d798e87ff570c9f57b1e72f81d
Gustav Lindqvist <gustav@glindqvist.se>
2014-01-27 13:06:58 +0100

    Create timeSince.js

---
+++ timeSince.js
@@ -0,0 +1,65 @@
+var timesince = function timeSince(selector) {
+
+    var templates = {
+        prefix: "",
+        future: "",
+        suffix: " sedan.",
+        seconds: "Mindre än en minut",
+        minute: "Ungefär en minut",
+        minutes: "%d minuter",
+        hour: "Ungefär en timme",
+        hours: "Ungefär %d timmar",
+        day: "En dag",
+        days: "%d dagar",
+        month: "Ungefär en månad",
+        months: "%d månader",
+        year: "Ungefär ett år",
+        years: "%d år"
+    };
+    var template = function(t, n) {
+        return templates[t] && templates[t].replace(/%d/i, Math.abs(Math.round(n)));
+    };
+
+    var timer = function(time) {
+        if (!time)
+            return;
+        time = parseInt(time) * 1000; // Change to milliseconds
+        time = new Date(time);
+        var now = new Date();
+        var seconds = ((now.getTime() - time) * .001) >> 0;
+        var minutes = seconds / 60;
+        var hours = minutes / 60;
+        var days = hours / 24;
+        var years = days / 365;
+
+        return templates.prefix + (
+                seconds < 0 && template('future') ||
+                seconds < 45 && template('seconds', seconds) ||
+                seconds < 90 && template('minute', 1) ||
+                minutes < 45 && template('minutes', minutes) ||
+                minutes < 90 && template('hour', 1) ||
+                hours < 24 && template('hours', hours) ||
+                hours < 42 && template('day', 1) ||
+                days < 30 && template('days', days) ||
+                days < 45 && template('month', 1) ||
+                days < 365 && template('months', days / 30) ||
+                years < 1.5 && template('year', 1) ||
+                template('years', years)
+                ) + (seconds < 0 && "" || templates.suffix);
+    };
+
+    var elements = document.getElementsByClassName('timesince');
+    for (var i in elements) {
+        var $this = elements[i];
+        if (typeof $this === 'object') {
+            $this.innerHTML = timer($this.dataset.timesince || $this.dataset.timesince);
+        }
+    }
+    // update time every minute
+    setTimeout(timesince, 60000);
+};
+
+
+
+
+    
